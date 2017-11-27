e1ec04165fc7a02822ed708b1b892c2addda6308
Artur Felipe Sousa <arturfelipe.sousa@gmail.com>
2017-11-27 11:17:09 -0200

    Disable the character accent menu

--- setup/darwin.sh
+++ setup/darwin.sh
@@ -3,3 +3,6 @@
 # Fast keyboard repeat rate
 defaults write -g InitialKeyRepeat -int 12
 defaults write -g KeyRepeat -int 1
+
+# Disable the character accent menu and enable key repeat
+defaults write -g ApplePressAndHoldEnabled -bool false
